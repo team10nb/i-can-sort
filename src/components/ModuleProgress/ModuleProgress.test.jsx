86b8488720600ac6ebdeadad9a8de98409763a0f
Colin <csl122@hotmail.com>
2021-01-25 15:28:47 +0800

    test(Module): optimise Module's test

--- src/components/ModuleProgress/ModuleProgress.test.jsx
+++ src/components/ModuleProgress/ModuleProgress.test.jsx
@@ -2,15 +2,18 @@ import { render } from "@testing-library/react";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import userEvent from "@testing-library/user-event";
 import { screen } from "@testing-library/react";
-import ModuleProgress from "./ModuleProgress";
+import * as ModuleProgress from "./ModuleProgress";
+import * as LinearProgresses from "@material-ui/core/LinearProgress";
+
 
 xit("should be value of 15", () => {
+    console.log(LinearProgresses)
     const borderLinearProgressSpy = jest
-        .spyOn(ModuleProgress.prototype, "BorderLinearProgress")
-        .mockImplementation(jest.fn());
+        .spyOn(LinearProgresses, "default")
+        .mockImplementation((props => <div>props.progress</div>));
 
     const props = { width: 200, progress: 10 };
-    const content = render(<ModuleProgress {...props} />);
+    const content = render(<ModuleProgress.default {...props} />);
     expect(borderLinearProgressSpy).toBeCalledTimes(1);
     expect(borderLinearProgressSpy).toBeCalledWith({
         variant: "determinate",
