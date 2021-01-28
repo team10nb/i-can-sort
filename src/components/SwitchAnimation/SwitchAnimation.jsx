c04056ee8f37302ab841f41b22be944005ab70d5
Colin <csl122@hotmail.com>
2021-01-28 20:28:34 +0800

    refactor(SwitchAnimation): last & next stepp will pause

--- src/components/SwitchAnimation/SwitchAnimation.jsx
+++ src/components/SwitchAnimation/SwitchAnimation.jsx
@@ -21,114 +21,113 @@ export const SwitchAnimation = () => {
     // }, [colors]);
 
     const clearTimeouts = () => {
-        timeOutIds.forEach((timeoutId) =>
-          clearTimeout(timeoutId)
-        );
+        timeOutIds.forEach((timeoutId) => clearTimeout(timeoutId));
         setTimeOutIds([]);
-      };
+    };
 
     const run = (trace) => {
         const timeoutIds = [];
         const timer = 500 / playSpeed;
-    
+
         // Set a timeout for each item in the trace
         trace.forEach((item, i) => {
-          let timeoutId = setTimeout(
-            (item, currentStep) => {
-                setCurrentStep( (currentStep) => (i === trace.length - 1 ) ? currentStep : currentStep + 1);
-                setColors(item);
-                (i === trace.length - 1 ) ? setIsPlaying(false) : setIsPlaying(true);
-            },
-            timer/3 + i * timer,
-            item, currentStep
-          );
-    
-          timeoutIds.push(timeoutId);
-        });
-    
-        // Clear timeouts upon completion
-        let timeoutId = setTimeout(
-          clearTimeouts,
-          trace.length * timer
-        );
-        timeoutIds.push(timeoutId);
-    
-        setTimeOutIds(timeoutIds);
-      };
+            let timeoutId = setTimeout(
+                (item, currentStep) => {
+                    setCurrentStep((currentStep) =>
+                        i === trace.length - 1 ? currentStep : currentStep + 1
+                    );
+                    setColors(item);
+                    i === trace.length - 1
+                        ? setIsPlaying(false)
+                        : setIsPlaying(true);
+                },
+                timer / 3 + i * timer,
+                item,
+                currentStep
+            );
 
-      const pause = () => {
+            timeoutIds.push(timeoutId);
+        });
+
+        // Clear timeouts upon completion
+        let timeoutId = setTimeout(clearTimeouts, trace.length * timer);
+        timeoutIds.push(timeoutId);
+
+        setTimeOutIds(timeoutIds);
+    };
+
+    const pause = () => {
         setIsPlaying(false);
         clearTimeouts();
-      };
+    };
 
-      const resume = () => {
-          setIsPlaying(true);
-          console.log(currentStep);
+    const resume = () => {
+        setIsPlaying(true);
+        console.log(currentStep);
         const newtrace = trace.slice(currentStep);
         run(newtrace);
-      };
+    };
 
-      const stepForward = () => {
+    const stepForward = () => {
         if (currentStep < trace.length - 1) {
-            console.log(currentStep);
-
-          const item = trace[currentStep + 1];
-
-          clearTimeouts();
-          setCurrentStep((prevStep) => prevStep + 1);
-          const newtrace = trace.slice(currentStep + 1);
-          
-          setColors(item);
-
-          run(newtrace);
+            pause();
+            const item = trace[currentStep + 1];
+            setCurrentStep((prevStep) => prevStep + 1);
+            setColors(item);
         }
-      };
-    
-      const stepBackward = () => {
+    };
+
+    const stepBackward = () => {
         if (currentStep > 0) {
-            console.log(currentStep);
-          const item = trace[currentStep - 1];
-          setCurrentStep(currentStep - 1);
-          setColors(item);
+            pause();
+            const item = trace[currentStep - 1];
+            setCurrentStep((prevStep) => prevStep - 1);
+            setColors(item);
         }
-      };
-    
+    };
+
     return (
         <div>
             <ul>
-            {colors.map((background) => (
-                <motion.li
-                    key={background.backgroundColor}
-                    layout
-                    transition={spring}
-                    style={background}
-                />
-            ))}
+                {colors.map((background) => (
+                    <motion.li
+                        key={background.backgroundColor}
+                        layout
+                        transition={spring}
+                        style={background}
+                    />
+                ))}
             </ul>
-            <button onClick={()=>{
-                setCurrentStep(0);
-                setColors(trace[0]);
-            }}>
+            <button
+                onClick={() => {
+                    setCurrentStep(0);
+                    setColors(trace[0]);
+                }}
+            >
                 Reset
             </button>
-            <button onClick={()=>{
-                isPlaying ? pause() : resume();
-            }}>
+            <button
+                onClick={() => {
+                    isPlaying ? pause() : resume();
+                }}
+            >
                 {isPlaying ? "pause" : "play"}
             </button>
-            <button onClick={()=>{
-                stepBackward();
-            }}>
+            <button
+                onClick={() => {
+                    stepBackward();
+                }}
+            >
                 Last step
             </button>
-            <button onClick={()=>{
-                stepForward();
-            }}>
+            <button
+                onClick={() => {
+                    stepForward();
+                }}
+            >
                 Next step
             </button>
-
         </div>
-        
     );
 };
 
@@ -197,11 +196,9 @@ const trace = [
         { height: "90px", backgroundColor: "#9C1AFF" },
     ],
 ];
-    
 
 // 可能可以以以下的形式来做每一个数字的object
 // { styles: {height: "50px", backgroundColor: "#D309E1"}, // style, 用来造bar
 //   key: 1, // 这个是用来识别是哪个物体的唯一标志
 //   value: 50, //数字的大小, 对应高度
 //  }
-
