f28f59d5d27bb11d5eced21413341e4f2020f55e
Colin <csl122@hotmail.com>
2021-01-29 15:51:11 +0800

    fix(SwitchAnimation): fix disable button not working

--- src/components/SwitchAnimation/SwitchAnimation.jsx
+++ src/components/SwitchAnimation/SwitchAnimation.jsx
@@ -17,7 +17,7 @@ const spring = {
     stiffness: 300, //这个是粘滞程度
 };
 export const SwitchAnimation = () => {
-    const [colors, setColors] = useState(trace[1]); // 初始状态不一样开始的时候会自然一点
+    const [colors, setColors] = useState(trace[0]); // 初始状态不一样开始的时候会自然一点
     const [playSpeed, setPlaySpeed] = useState(1);
     const [currentStep, setCurrentStep] = useState(0);
     const [timeOutIds, setTimeOutIds] = useState([]);
@@ -26,14 +26,15 @@ export const SwitchAnimation = () => {
     const [playDisabled, setPlayDisabled] = useState(false);
     const [backwardDisabled, setBackwardDisabled] = useState(true);
 
-    let stepCounter = 0;
-    // let clicked = 0;
-    // const [forwardDisabled, setForwardDisabled] = useState(false);
-    // const [pauseDisabled, setPauseDisabled] = useState(false);
+    useEffect(() => {
+        currentStep === 0
+            ? setBackwardDisabled(true)
+            : setBackwardDisabled(false);
+        currentStep + 1 === trace.length
+            ? setPlayDisabled(true)
+            : setPlayDisabled(false);
+    }, [currentStep]);
 
-
-
-// <<<<<<< Updated upstream
     //speed变的时候要重新播放一下, 改变速度
     useEffect(() => {
         if (isPlaying) {
@@ -54,22 +55,6 @@ export const SwitchAnimation = () => {
         }
         setAnchorEl(null);
     };
-// =======
-   
-   
-    // useEffect(() => {
-    //     setTimeout(() => setColors(shuffle(colors)), 1000);
-    // }, [colors]);
-// >>>>>>> Stashed changes
-
-    const checkDisabled = (step) => {
-        step === 0 ? setBackwardDisabled(true) : setBackwardDisabled(false);
-        step === trace.length ? setPlayDisabled(true) : setPlayDisabled(false); 
-    }
-
-    // const checkDisabledClick = (clicked) => {
-    //     clicked === -1
-    // }
 
     const clearTimeouts = () => {
         timeOutIds.forEach((timeoutId) => clearTimeout(timeoutId));
@@ -77,30 +62,23 @@ export const SwitchAnimation = () => {
     };
 
     const run = (trace) => {
+        const subTrace = trace.slice(1);
         const timeoutIds = [];
         const timer = 500 / playSpeed;
 
         // Set a timeout for each item in the trace
-        trace.forEach((item, i) => {
+        subTrace.forEach((item, i) => {
             let timeoutId = setTimeout(
                 (item, currentStep) => {
                     setCurrentStep((currentStep) =>
                         i === trace.length - 1 ? currentStep : currentStep + 1
                     );
                     setColors(item);
-                    i === trace.length - 1
+                    i === subTrace.length - 1
                         ? setIsPlaying(false)
                         : setIsPlaying(true);
-                    
-                        
-                    stepCounter++;
-                    checkDisabled(stepCounter);
-                    console.log(stepCounter);
-                    // console.log(currentStep);
-                    // i > 0
-                    //     ? setBackwardDisabled(false)
-                    //     : setBackwardDisabled(true);
-                    
+
+                    console.log(subTrace.length - 1);
                 },
                 i * timer,
                 item,
@@ -134,17 +112,10 @@ export const SwitchAnimation = () => {
             const item = trace[currentStep + 1];
             setCurrentStep((prevStep) => prevStep + 1);
             setColors(item);
-            stepCounter++;
-            console.log(stepCounter);
-
-            checkDisabled(stepCounter);
-
-
-            
         }
-        currentStep === trace.length ? setPlayDisabled(true):setPlayDisabled(false);
-
-        
+        currentStep + 1 === trace.length
+            ? setPlayDisabled(true)
+            : setPlayDisabled(false);
     };
 
     const stepBackward = () => {
@@ -153,17 +124,8 @@ export const SwitchAnimation = () => {
             const item = trace[currentStep - 1];
             setCurrentStep((prevStep) => prevStep - 1);
             setColors(item);
-            stepCounter--;
-            // console.log(stepCounter);
-            // console.log(currentStep);
-
-            checkDisabled(stepCounter);
         }
-        currentStep === 1 ? setBackwardDisabled(true):setBackwardDisabled(false);
-        
     };
-
-   
 
     return (
         <div>
@@ -178,126 +140,89 @@ export const SwitchAnimation = () => {
                     />
                 ))}
             </ul>
-{/* <<<<<<< Updated upstream */}
             <Tooltip title='Replay'>
-                <IconButton
-                    onClick={() => {
-                        // pause(); reset时候是否需要也pause一下？
-                        setCurrentStep(0);
-                        setColors(trace[0]);
-                        stepCounter = 0;
-                        checkDisabled(stepCounter);
-                    }}
-                    disabled={false}
-                >
-                    <ReplayIcon style={{ color: "grey" }} fontSize='small' />
-                </IconButton>
+                <span>
+                    <IconButton
+                        onClick={() => {
+                            pause();
+                            setCurrentStep(0);
+                            setColors(trace[0]);
+                        }}
+                        disabled={false}
+                    >
+                        <ReplayIcon
+                            style={{ color: "grey" }}
+                            fontSize='small'
+                        />
+                    </IconButton>
+                </span>
             </Tooltip>
 
             <Tooltip title='Previous Step'>
-                <IconButton
-                    onClick={() => {
-                        stepBackward();
-                    }}
-                    disabled={backwardDisabled}
-                >
-                    <SkipPreviousIcon
-                        style={{ color: "grey" }}
-                        fontSize='small'
-                    />
-                </IconButton>
+                <span>
+                    <IconButton
+                        onClick={() => {
+                            stepBackward();
+                        }}
+                        disabled={backwardDisabled}
+                    >
+                        <SkipPreviousIcon
+                            style={{ color: "grey" }}
+                            fontSize='small'
+                        />
+                    </IconButton>
+                </span>
             </Tooltip>
 
             <Tooltip title={isPlaying ? "Pause" : "Play"}>
-                <IconButton
-                    onClick={() => {
-                        isPlaying ? pause() : resume();
-                    }}
-                    disabled={playDisabled}
-                >
-                    {isPlaying ? (
-                        <PauseCircleFilledIcon fontSize='large' />
-                    ) : (
-                        <PlayCircleFilledIcon fontSize='large' />
-                    )}
-                </IconButton>
+                <span>
+                    <IconButton
+                        onClick={() => {
+                            isPlaying ? pause() : resume();
+                        }}
+                        disabled={playDisabled}
+                    >
+                        {isPlaying ? (
+                            <PauseCircleFilledIcon fontSize='large' />
+                        ) : (
+                            <PlayCircleFilledIcon fontSize='large' />
+                        )}
+                    </IconButton>
+                </span>
             </Tooltip>
 
             <Tooltip title='Next Step'>
-                <IconButton
-                    onClick={() => {
-                        stepForward();
-                    }}
-                    disabled={playDisabled}
-                >
-                    <SkipNextIcon style={{ color: "grey" }} fontSize='small' />
-                </IconButton>
+                <span>
+                    <IconButton
+                        onClick={() => {
+                            stepForward();
+                        }}
+                        disabled={playDisabled}
+                    >
+                        <SkipNextIcon
+                            style={{ color: "grey" }}
+                            fontSize='small'
+                        />
+                    </IconButton>
+                </span>
             </Tooltip>
-            
-                <Menu
-                    handleClick={handleClick}
-                    handleClose={handleClose}
-                    anchorEl={anchorEl}
-                    speed={playSpeed + "x"}
-                />
-            
+
+            <Menu
+                handleClick={handleClick}
+                handleClose={handleClose}
+                anchorEl={anchorEl}
+                speed={playSpeed + "x"}
+            />
+
             <br />
             <Tooltip title='Progress'>
-                <AnimationProgress
-                    width='270px'
-                    progress={(100 * currentStep) / (trace.length - 1)}
-                />
+                <span>
+                    <AnimationProgress
+                        width='270px'
+                        progress={(100 * currentStep) / (trace.length - 1)}
+                    />
+                </span>
             </Tooltip>
-=======
-            {/* <button
-                onClick={() => {
-                    pause();
-                    setCurrentStep(0);
-                    setColors(trace[0]);
-                    stepCounter = 0;
-                    checkDisabled(stepCounter);
-                }}
-            >
-                Reset
-            </button>
-
-            <button
-                onClick={() => {
-                    isPlaying ? pause() : resume();
-                }}
-                
-                // disabled={currentStep === trace.length - 1}
-                disabled={playDisabled}
-                // disabled={stepCounter === trace.length-1}
-
-
-
-        
-            >
-                {isPlaying ? "pause" : "play"}
-            </button>
-
-            <button
-                onClick={() => {
-                    stepBackward();
-                }}
-                disabled={backwardDisabled}
-                // disabled={currentStep === 0}
-            >
-                Last step
-            </button>
-
-            <button
-                onClick={() => {
-                    stepForward();
-                }}
-                disabled={playDisabled}
-                // disabled={currentStep === trace.length - 1}
-                // disabled={stepCounter === trace.length-1}
-            >
-                Next step
-            </button> */}
-{/* >>>>>>> Stashed changes */}
         </div>
     );
 };
