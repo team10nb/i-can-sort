a522453b15a4304d64ccbac49cc9a84e780a4838
scyyj2 <scyyj2@nottingham.edu.cn>
2021-01-30 20:01:17 +0800

    fix(Routers): add new routes to subpages.

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -5,10 +5,10 @@ import Setting from './scenes/Setting';
 import { Switch } from 'react-router-dom';
 import Help from './scenes/Help';
 import Exercise from './scenes/Exercise';
-import SubScenes from './subScenes/SubScenes';
-import ProcedureMainPage from './scenes/MainPages/Procedure';
-import EfficiencyMainPage from './scenes/MainPages/Efficiency';
-import CorrectnessMainPage from './scenes/MainPages/Correctness';
+import CorrectnessMainPage from './scenes/mainPages/Correctness';
+import ProcedureMainPage from './scenes/mainPages/Procedure';
+import EfficiencyMainPage from './scenes/mainPages/Efficiency';
+import ProcedureRouters from './scenes/subPages/Procedure/ProcedureRouters';
 
 export default function Routers() {
     return(
@@ -18,12 +18,27 @@ export default function Routers() {
             <Route path="/Tutorial" component={Tutorial} />
             <Route path="/Setting" component={Setting} />
             <Route path="/Help" component={Help} />
+            {/* <Route path="/ProcedureMainPage" render={props => <ProcedureMainPage algoProcess="0" {...props}/>} /> */}
             <Route path="/ProcedureMainPage" component={ProcedureMainPage} />
             <Route path="/EfficiencyMainPage" component={EfficiencyMainPage} />
             <Route path="/CorrectnessMainPage" component={CorrectnessMainPage} />
             <Route path="/Exercise" component={Exercise} />
-            <Route path="/Page" component={SubScenes} />
-            <Route path="/" component={ProcedureMainPage} />
+            <Route path="/ProcedureSubPage"
+                render={() => {
+                    return(
+                        <Switch>
+                            {ProcedureRouters.map((route, algorithm) => (
+                                <Route
+                                key={algorithm}
+	                        	path={route.path}
+	                        	component={route.component}
+                                />
+                            ))}
+                        </Switch>
+                    );
+                }}
+            ></Route>
+            <Route path="/" component={ProcedureMainPage}/>
             </Switch>
             </div>
         </Router>
