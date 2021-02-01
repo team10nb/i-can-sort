e2c5cbe9d52d8c3bfe186374ad67efa918bd519d
Colin <csl122@hotmail.com>
2021-02-01 14:23:46 +0800

    test(AnimationControl): add test for buttons

--- src/components/AnimationControl/AnimationControl.test.jsx
+++ src/components/AnimationControl/AnimationControl.test.jsx
@@ -1,3 +1,41 @@
-test('should ', () => {
-    expect(true).toBeTruthy();
+import { render } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import AnimationControl from "./AnimationControl";
+
+const handleResetClick = jest.fn();
+const stepForward = jest.fn();
+const stepBackward = jest.fn();
+const pause = jest.fn();
+const resume = jest.fn();
+const handleClick = jest.fn();
+const handleClose = jest.fn();
+
+
+const props = { handleResetClick, stepForward, stepBackward, pause, resume, handleClick, handleClose};
+
+
+
+test('should have 5 buttons', () => {
+    const {getAllByRole} = render(<AnimationControl {...props}/>);
+    const buttons = getAllByRole('button');
+    expect(buttons.length).toBe(5);
 })
+
+test('should call functions', () => {
+    const {getAllByRole} = render(<AnimationControl {...props}/>);
+    const buttons = getAllByRole('button');
+
+    userEvent.click(buttons[0]);
+    userEvent.click(buttons[1]);
+    userEvent.click(buttons[2]);
+    userEvent.click(buttons[3]);
+    userEvent.click(buttons[4]);
+
+    expect(handleResetClick).toBeCalledTimes(1);
+    expect(stepForward).toBeCalledTimes(1);
+    expect(stepBackward).toBeCalledTimes(1);
+    expect(resume).toBeCalledTimes(1);
+    expect(handleClick).toBeCalledTimes(1);
+    
+})
+
