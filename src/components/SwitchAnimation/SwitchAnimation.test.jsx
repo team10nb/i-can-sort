1d4bc4349969a917bef0f0e3354d416e64ffd549
Colin <csl122@hotmail.com>
2021-02-04 23:31:36 +0800

    test(SwitchAnimation): fix passing property issue

--- src/components/SwitchAnimation/SwitchAnimation.test.jsx
+++ src/components/SwitchAnimation/SwitchAnimation.test.jsx
@@ -10,6 +10,12 @@ const trace = [
         { height: "90px", backgroundColor: "#9C1AFF", key: '3' },
         { height: "10px", backgroundColor: "#7700FF", key: '4' },
         { height: "20px", backgroundColor: "#66AA00", key: '5' },
+    ],[
+        { height: "50px", backgroundColor: "#FF008C", key: '1' },
+        { height: "30px", backgroundColor: "#D309E1", key: '2' },
+        { height: "90px", backgroundColor: "#9C1AFF", key: '3' },
+        { height: "10px", backgroundColor: "#7700FF", key: '4' },
+        { height: "20px", backgroundColor: "#66AA00", key: '5' },
     ],
 
 ];
@@ -23,7 +29,7 @@ test('should call AnimationSlider', () => {
     const sliderSpy = jest.spyOn(AnimationSliders, "default").mockImplementation(() => <div>mockAnimationSlider</div>);
     const content = render(<SwitchAnimation {...props}/>);
     content.getByText("mockAnimationSlider");
-    expect(sliderSpy).toBeCalledTimes(2);
+    expect(sliderSpy).toBeCalledTimes(1);
 })
 
 test('should call AnimationControl', () => {
@@ -32,7 +38,7 @@ test('should call AnimationControl', () => {
 
     const {getByText} = render(<SwitchAnimation {...props}/>);
     getByText("mockAnimationControl");
-    expect(controlSpy).toBeCalledTimes(2);
+    expect(controlSpy).toBeCalledTimes(1);
 });
 
 test('should match snapshot', () => {
