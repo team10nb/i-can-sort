862ff9fc6fc8317a73123ee7f5737dd9891b0d44
biyrh2 <biyrh2@nottingham.edu.cn>
2021-02-16 19:35:11 +0800

    test(SwitchSort): add test, snapshot and plan.

---
+++ src/components/SwitchSort/SwitchSort.test.jsx
@@ -0,0 +1,33 @@
+import { render } from "@testing-library/react";
+import SwitchSort from "./SwitchSort";
+import * as InputBar from "../InputBar/InputBar";
+import * as SwitchAnimation from "../SwitchAnimation/SwitchAnimation";
+
+const props = {
+    sort: 'Bubble',
+};
+
+it('should call InputBar', () => {
+    jest.restoreAllMocks();
+    const inputSpy = jest.spyOn(InputBar, "default").mockImplementation(() => <div>mockInputBar</div>);
+    const {getByText} = render(<SwitchSort {...props} />);
+    getByText("mockInputBar");
+    expect(inputSpy).toBeCalledTimes(1);
+})
+
+it('should call SwitchAnimation', () => {
+    jest.restoreAllMocks();
+    const switchSpy = jest.spyOn(SwitchAnimation, "default").mockImplementation(() => <div>mockSwitchAnimation</div>);
+    const {getByText} = render(<SwitchSort {...props} />);
+    getByText("mockSwitchAnimation");
+    expect(switchSpy).toBeCalledTimes(1);
+})
+
+it('should match snapshot', () => {
+    jest.restoreAllMocks();
+    const content = render(<SwitchSort {...props} />);
+    expect(content).toMatchSnapshot();
+})
+
+
+
---
+++ src/components/SwitchSort/TestPlan_SwitchSort.md
@@ -0,0 +1,11 @@
+## Test Plan 
+--------
+<br>
+
+#### Component: SwitchSort
+
+|Test Name|Description|Operation|Expected Outcome|
+|----|------|--------|----------------|
+|should call InputBar||||
+|should call SwitchAnimation||||
+| should match Snapshot | Compare new snapshot with old one. |  | Snapshots are the same. |
