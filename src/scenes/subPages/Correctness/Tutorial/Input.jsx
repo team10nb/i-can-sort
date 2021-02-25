da05f3a96b54c9349dbd17aaf787cdb95f04a32b
Colin <csl122@hotmail.com>
2021-02-25 20:24:03 +0800

    feat(InputTutorial): add error message for input

--- src/scenes/subPages/Correctness/Tutorial/Input.jsx
+++ src/scenes/subPages/Correctness/Tutorial/Input.jsx
@@ -11,6 +11,7 @@ import CardContent from "@material-ui/core/CardContent";
 import InputBox from "../../../../components/InputBox/InputBox";
 import ExplainationBox from "../../../../components/ExplainationBox/ExplainationBox";
 import { motion } from "framer-motion";
+import ErrorIcon from "@material-ui/icons/Error";
 
 // a framer motion transition attributes
 const spring = {
@@ -19,7 +20,6 @@ const spring = {
     stiffness: 150, //Stiffness of the spring. Higher values will create more sudden movement. Set to 100 by default.
     mass: 0.1, // Mass of the moving object. Higher values will result in more lethargic movement
 };
-
 
 const useStyles = makeStyles((theme) => ({
     div: {
@@ -34,6 +34,8 @@ const useStyles = makeStyles((theme) => ({
         background: "#F0F0F0",
         width: 550,
         height: 530,
+        display: "grid",
+        justifyContent: "center",
     },
 
     InputBox: {
@@ -76,11 +78,12 @@ const useStyles = makeStyles((theme) => ({
     },
     switchAnimation: {
         display: "grid",
-            justifyContent: "center",
-            alignContent: "center",
-            height: 430,
-            marginTop: "20px",
-    }
+        justifyContent: "center",
+        alignContent: "flex-end",
+        marginTop: "100px",
+        height: "300px",
+        width: "300px",
+    },
 }));
 
 export default function Input(props) {
@@ -95,98 +98,64 @@ export default function Input(props) {
         false,
         false,
     ]);
+    const [isLegalPlaying, setIsLegalPlaying] = useState(true);
     const legalArrays = [
         [1, 2, 3, 4],
         [6, 3, 1, 7],
         [8, 5, 4, 7],
     ];
     const illegalArrays = [
-        [1, 2, 3, 4],
-        [1, 2, 3, 5],
-        [1, 2, 3, 6],
+        ["a", "b", "c", "d"],
+        ["e", "f", "g", "h"],
+        ["i", "j", "k", "l"],
     ];
     // The bars displayed to visulise the numbers
     const [bars, setBars] = useState(legalArrays[0]);
 
-    
-    
-
     const legalTimeoutIds = [];
     const illegalTimeoutIds = [];
 
-
     const legalClick = () => {
+        setIsLegalPlaying(true);
         setBars(legalArrays[0]);
-        setIsCompleteLegal([false, false, false])
+        setIsCompleteLegal([false, false, false]);
 
         legalArrays.forEach((item, i) => {
             let timeoutId = setTimeout(
                 (item) => {
                     if (i === 0) {
-                        setIsCompleteLegal([true, false, false])
-                        setBars(item);
-                    } else if (i===1){
-                        setIsCompleteLegal([true, true, false])
-                        setBars(item);
-                    } else if (i===2){
-                        setIsCompleteLegal([true, true, true])
-                        setBars(item);
+                        setIsCompleteLegal([true, false, false]);
+                    } else if (i === 1) {
+                        setIsCompleteLegal([true, true, false]);
+                    } else if (i === 2) {
+                        setIsCompleteLegal([true, true, true]);
                     }
-                    
+                    setBars(item.sort((a, b) => a - b));
                 },
-                (i+1) * 700, //time interval
+                (i + 1) * 700, //time interval
                 item
             );
             legalTimeoutIds.push(timeoutId);
-            timeoutId = setTimeout(
-                (item) => {
-                    setBars(item.sort((a, b)=>a-b));
-                    
-                },
-                200, //time interval
-                item
-            );
-            legalTimeoutIds.push(timeoutId);
-            
         });
     };
 
     const illegalClick = () => {
-        
-        setIsCompleteIllegal([false, false, false])
-        legalArrays.forEach((item, i) => {
+        setIsLegalPlaying(false);
+        setBars(illegalArrays[0]);
+        setIsCompleteIllegal([false, false, false]);
+        illegalArrays.forEach((item, i) => {
             let timeoutId = setTimeout(
                 (item) => {
                     if (i === 0) {
-                        setIsCompleteIllegal([true, false, false])
-                        setBars(item);
-                    } else if (i===1){
-                        setIsCompleteIllegal([true, true, false])
-                        setBars(item);
-                    } else if (i===2){
-                        setIsCompleteIllegal([true, true, true])
-                        setBars(item);
+                        setIsCompleteIllegal([true, false, false]);
+                    } else if (i === 1) {
+                        setIsCompleteIllegal([true, true, false]);
+                    } else if (i === 2) {
+                        setIsCompleteIllegal([true, true, true]);
                     }
+                    setBars(item);
                 },
-                (i+1) * 700, //time interval
-                item
-            );
-            illegalTimeoutIds.push(timeoutId);
-
-            timeoutId = setTimeout(
-                (item) => {
-                    if (i === 0) {
-                        setIsCompleteIllegal([true, false, false])
-                        setBars(item.sort((a, b)=>a-b));
-                    } else if (i===1){
-                        setIsCompleteIllegal([true, true, false])
-                        setBars(item.sort((a, b)=>a-b));
-                    } else if (i===2){
-                        setIsCompleteIllegal([true, true, true])
-                        setBars(item.sort((a, b)=>a-b));
-                    }
-                },
-                (i+1) * 700 + 350, //time interval
+                (i + 1) * 700, //time interval
                 item
             );
             illegalTimeoutIds.push(timeoutId);
@@ -196,27 +165,40 @@ export default function Input(props) {
     return (
         <div className={classes.div}>
             <Card className={classes.card}>
-                <CardContent className={classes.switchAnimation}>
-                <div className={classes.bars}>
-                {bars.map((value, key) => (
-                    <motion.li
-                        key={key} // each bar's identification
-                        layout
-                        transition={spring}
-                        style={{backgroundColor: "#00BFFF", height:value*10+"px"}}
-                        className={classes.bar}
-                    >
-                        <div className={classes.barNumber}>
-                            {value}
-                        </div>
-
-                    </motion.li>
-                ))}
-            </div>
-            <ExplainationBox width='30' height={3}>
-                {bars.toString()}
-            </ExplainationBox>
-                </CardContent>
+                <div className={classes.switchAnimation}>
+                    <div className={classes.bars}>
+                        {isLegalPlaying ? (
+                            bars.map((value, key) => (
+                                <motion.li
+                                    key={key} // each bar's identification
+                                    layout
+                                    transition={spring}
+                                    style={{
+                                        backgroundColor: "#00BFFF",
+                                        height: value * 10 + "px",
+                                    }}
+                                    className={classes.bar}
+                                >
+                                    <div className={classes.barNumber}>
+                                        {value}
+                                    </div>
+                                </motion.li>
+                            ))
+                        ) : (
+                            <div style={{display: "grid",
+                            justifyContent: "center",
+                            alignContent: "flex-end"}}>
+                                <ErrorIcon style={{ fontSize: 60, color: "red" }} />
+                                
+                            </div>
+                            
+                        )}
+                    </div>
+                    {isLegalPlaying? "" : <span style={{fontWeight:"600", marginBottom:"8px"}}>Error! Could not parse ILLEGAL input</span>}
+                    <ExplainationBox width="30" height={3}>
+                        {bars.toString()}
+                    </ExplainationBox>
+                </div>
             </Card>
             <InputBox
                 className={classes.InputBox}
