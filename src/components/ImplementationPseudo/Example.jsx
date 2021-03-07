9ccb1d762e424511aebd92aa8d7fd1754df4308f
Yani <scyyh7@nottingham.edu.cn>
2021-03-07 18:10:20 +0800

    refactor: add blockNums trace

--- src/components/ImplementationPseudo/Example.jsx
+++ src/components/ImplementationPseudo/Example.jsx
@@ -6,6 +6,25 @@ const BubbleExample = () => {
     const Code = Bubble;
     const sort = 'Bubble Sort';
     const arr = [4, 8, 11, 13, 5];
+    // const blockNums = [1, 4, 3, 4, 3, 4, 3, 4, 5, 7, 4, 3, 4, 3, 4, 5, 7, 4, 3, 4, 5, 7, 4, 3, 7, 7, 8];
+    const {trace, description, blockNums} = BubbleSort(arr);
+    
+    const props = {
+        sort,
+        trace,
+        description,
+        blockNums,
+        Code,
+    };
+
+    return <ImplementationPseudo {...props}/>
+}
+
+
+const SelectionExample = () => {
+    const Code = Bubble;
+    const sort = 'Bubble Sort';
+    const arr = [4, 8, 11, 13, 5];
     const blockNums = [1, 4, 3, 4, 3, 4, 3, 4, 5, 7, 4, 3, 4, 3, 4, 5, 7, 4, 3, 4, 5, 7, 4, 3, 7, 7, 8];
     const {trace, description} = BubbleSort(arr);
     
@@ -20,6 +39,8 @@ const BubbleExample = () => {
     return <ImplementationPseudo {...props}/>
 }
 
+
 export {
     BubbleExample,
+    SelectionExample
 };
