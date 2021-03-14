78d27226bf2394d975002072e50ef0ab10c590ee
biyrh2 <biyrh2@nottingham.edu.cn>
2021-03-15 00:28:35 +0800

    fix(Loop): adjust css

--- src/components/LoopExample/WhileLoop.jsx
+++ src/components/LoopExample/WhileLoop.jsx
@@ -5,65 +5,7 @@
 import { makeStyles } from "@material-ui/core/styles";
 import * as React from "react";
 import AnimationCode from '../AnimationCode/AnimationCode';
-
-const useStyles = makeStyles((theme) =>({
-    root: {
-        '& > * + *': {     
-            marginTop: theme.spacing(3),
-        },
-        width: 380,    
-    },
-    bars: {
-        listStyle: "none",
-        padding: 0,
-        margin: 0,
-        position: "relative",
-        display: "flex",
-        flexWrap: "wrap-reverse",
-        justifyContent: "center",
-    },
-    bar: {
-        listStyle: "none",
-        padding: 0,
-        margin: 0,
-        boxShadow: "0px 0px 2px 2px #88888833",
-        borderRadius: "10px",
-        marginTop: 32,
-        marginBottom: "5px",
-        marginRight: "15px",
-        marginLeft: "15px",
-        width: "30px",
-        height: "140px",
-        fontSize: "0.1em",
-        justifyContent: "center",
-    },
-    barNumber: {
-        fontSize: "16px",
-        marginTop: "-23px",
-        textAlign: "center",
-        fontWeight: "600",
-    },
-    cardOne:{
-        width: 380, 
-        height: 240
-    },
-    cardTwo:{
-        width: 380, 
-        height: 240,
-        background: "#F0F0F0",        
-    },
-    slider:{
-        marginLeft: 28,
-        marginBottom: 0,
-    },
-    title:{
-        display:"flex",
-        fontFamily: "inherit",
-        fontWeight: "700",
-        fontSize:"16px",
-        justifyContent: "center",
-    },
-}));
+import { useStyles } from './Style';
 
 // dynamic pseudocode
 const Code = (props)=> {
