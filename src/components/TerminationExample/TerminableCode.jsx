10507b8dc6c797e1fb510e35914ddda002b5a354
biyrh2 <biyrh2@nottingham.edu.cn>
2021-03-27 21:25:46 +0800

    feat(Termination): change terminating example

--- src/components/TerminationExample/TerminableCode.jsx
+++ src/components/TerminationExample/TerminableCode.jsx
@@ -7,7 +7,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 import * as React from "react";
 import AnimationWithCode from '../AnimationWithCode/AnimationWithCode';
-import BubbleSort from '../SwitchAnimation/SortingAlgorithms/BubbleSort/BubbleSort';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -31,8 +30,8 @@ const useStyles = makeStyles((theme) => ({
         margin: 0,
         boxShadow: "0px 0px 2px 2px #88888833",
         borderRadius: "10px",
-        marginTop: 32,
-        marginBottom: "5px",
+        marginTop: 50,
+        marginBottom: "-5px",
         marginRight: "15px",
         marginLeft: "15px",
         width: "30px",
@@ -49,7 +48,7 @@ const useStyles = makeStyles((theme) => ({
     cardOne: {
         width: 380,
         height: 240,
-        background: "#F0F0F0",
+        background: "#F0F0F0"
     },
     cardTwo: {
         width: 380,
@@ -58,7 +57,7 @@ const useStyles = makeStyles((theme) => ({
     },
     slider: {
         marginLeft: 28,
-        marginBottom: 0,
+        marginTop: 10,
     },
     title: {
         display: "flex",
@@ -66,9 +65,18 @@ const useStyles = makeStyles((theme) => ({
         fontWeight: "700",
         fontSize: "16px",
         justifyContent: "center",
+        marginBottom: -5,
     },
-    box: {
-        marginTop: -60,
+    explanation: {
+        fontFamily: "inherit",
+        fontSize: "16px",
+        fontWeight: "700",
+        letterSpacing: "1px",
+        textAlign: 'center',
+        marginBottom: -8,
+    },
+    control: {
+        marginTop: -10,
     }
 }));
 
@@ -98,39 +106,22 @@ const Code = (props) => {
     }));
     const classes = useStyles();
 
-
-
     const terminableCode =
         <div>
             <pre className={blockNum === 1 ? classes.background : classes.noBackground}>
-                {`Algorithm: BubbleSort(Arr)`}
+                {`Algorithm: SwapFourTimes(Arr)`}
             </pre>
             <pre className={classes.noBackground}>
-                {`  length = Arr.length`}
+                {`  i = 0;`}
             </pre>
             <pre className={blockNum === 2 ? classes.background : classes.noBackground}>
-                {`  for i from 0 to length-1 do`}
+                {`  for i from 0 to 3 do`}
             </pre>
             <pre className={blockNum === 3 ? classes.background : classes.noBackground}>
-                {`    for j from 0 to length-1-i do`}
+                {`    swap(Arr[0], Arr[1])`}
             </pre>
             <pre className={blockNum === 4 ? classes.background : classes.noBackground}>
-                {`      if Arr[j] > Arr[j+1] then`}
-            </pre>
-            <pre className={blockNum === 5 ? classes.background : classes.noBackground}>
-                {`        swap((Arr[j], Arr[j+1])`}
-            </pre>
-            <pre className={blockNum === 6 ? classes.background : classes.noBackground}>
-                {`      end if`}
-            </pre>
-            <pre className={blockNum === 7 ? classes.background : classes.noBackground}>
-                {`    end for`}
-            </pre>
-            <pre className={blockNum === 8 ? classes.background : classes.noBackground}>
                 {`  end for`}
-            </pre>
-            <pre className={blockNum === 9 ? classes.background : classes.noBackground}>
-                {`return Arr`}
             </pre>
         </div>
 
@@ -141,18 +132,46 @@ const Code = (props) => {
 
 // algorithm can terminate example
 const TerminableCode = () => {
-    // example array
-    const arr = [2, 6, 9, 10, 3];
-    const { trace, blockNums } = BubbleSort(arr);
+
+    const trace = [];
+    const traceUnit1 =
+        [
+            { value: 5, height: 50, backgroundColor: "#00BFFF", key: 0, y: 0 },
+            { value: 8, height: 80, backgroundColor: "#00BFFF", key: 1, y: 0 },
+        ];
+
+    const traceUnit2 =
+        [
+            { value: 8, height: 80, backgroundColor: "#00BFFF", key: 1, y: 0 },
+            { value: 5, height: 50, backgroundColor: "#00BFFF", key: 0, y: 0 },
+        ];
+
+
+    trace.push(traceUnit1, traceUnit1, traceUnit2, traceUnit2, traceUnit1, traceUnit1, traceUnit2, traceUnit2, traceUnit1, traceUnit1);
+
+    const blockNums = [1, 2, 3, 2, 3, 2, 3, 2, 3, 4];
+
+    const description = [
+        "i = 0",
+        "i = 0",
+        "i = 0",
+        "i = 1",
+        "i = 1",
+        "i = 2",
+        "i = 2",
+        "i = 3",
+        "i = 3",
+        "i = 4, out of loop",
+    ];
 
     const props = {
         trace,
-        description: "",
+        description,
         width: 320,
         blockNums,
         useStyles,
         Code,
-        title: "Terminating Bubble Sort",
+        title: "Terminating Swap",
     };
 
     return (<AnimationWithCode {...props} />);
