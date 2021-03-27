6f391c756ffb0cce1c17723c081ba79f41d21ace
biyrh2 <biyrh2@nottingham.edu.cn>
2021-03-27 21:26:11 +0800

    feat(Termination): add tipframe

--- src/scenes/subPages/Correctness/Tutorial/Termination.jsx
+++ src/scenes/subPages/Correctness/Tutorial/Termination.jsx
@@ -6,11 +6,13 @@ import React from 'react';
 import { makeStyles } from "@material-ui/core/styles";
 import { TerminableCode } from '../../../../components/TerminationExample/TerminableCode';
 import InterminableCode from '../../../../components/TerminationExample/InterminableCode';
+import TipFrame from '../../../../components/TipFrame/TipFrame';
+import { color } from '../../../mainPages/Correctness';
 
 const useStyles = makeStyles((theme) => ({
-    root:{
+    root: {
         display: "flex",
-        '& > * + *': {     
+        '& > * + *': {
             marginLeft: theme.spacing(2),
         },
     },
@@ -23,36 +25,51 @@ const InterminablePart = () => {
     // loop trace
     const trace = [
         [
-            {value: 5, height: 50, backgroundColor: "#00BFFF", key: 0, y: 0},
-            {value: 10, height: 100, backgroundColor: "#00BFFF", key: 1, y: 0}
-        ],[
-            {value: 5, height: 50, backgroundColor: "#FFD700", key: 0, y: 0},
-            {value: 10, height: 100, backgroundColor: "#FFD700", key: 1, y: 0}
-        ],[
-            {value: 10, height: 100, backgroundColor: "#00BFFF", key: 1, y: 0},
-            {value: 5, height: 50, backgroundColor: "#00BFFF", key: 0, y: 0}
-        ],[
-            {value: 10, height: 100, backgroundColor: "#FFD700", key: 1, y: 0},
-            {value: 5, height: 50, backgroundColor: "#FFD700", key: 0, y: 0}
+            { value: 5, height: 50, backgroundColor: "#00BFFF", key: 0, y: 0 },
+            { value: 8, height: 80, backgroundColor: "#00BFFF", key: 1, y: 0 }
+        ], [
+            { value: 5, height: 50, backgroundColor: "#FFD700", key: 0, y: 0 },
+            { value: 8, height: 80, backgroundColor: "#FFD700", key: 1, y: 0 }
+        ], [
+            { value: 8, height: 80, backgroundColor: "#00BFFF", key: 1, y: 0 },
+            { value: 5, height: 50, backgroundColor: "#00BFFF", key: 0, y: 0 }
+        ], [
+            { value: 8, height: 80, backgroundColor: "#FFD700", key: 1, y: 0 },
+            { value: 5, height: 50, backgroundColor: "#FFD700", key: 0, y: 0 }
         ]
     ];
-    
+
     const props = {
         trace,
         explainationBoxHeight: 3,
         blockNums
     };
 
-    return(<InterminableCode {...props}/>);
+    return (<InterminableCode {...props} />);
 }
 
 export default function Termination() {
     const classes = useStyles();
 
-    return(
-        <div className = {classes.root}>
-            <TerminableCode/>
-            <InterminablePart/>
+    const message = <p>Termination is an important concept in <font style={{ fontWeight: 'bold' }}>algorithm correctness</font>. Here are two examples, which may help you understand <font style={{ fontWeight: 'bold' }}>what is termination.</font></p>
+
+    const size = {
+        left: 240,
+        top: 45,
+        bottom: 45,
+    };
+
+    return (
+        <div>
+            <TipFrame
+                message={message}
+                size={size}
+                btnColor={color}
+            />
+            <div className={classes.root}>
+                <TerminableCode />
+                <InterminablePart />
+            </div>
         </div>
     );
 }
